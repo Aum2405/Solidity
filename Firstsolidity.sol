b1b91e318263129fc613ca4f44061937dc098705
Aum2405 <88958216+Aum2405@users.noreply.github.com>
2023-06-22 22:28:26 +0530

    Firstsolidity.sol

--- Firstsolidity.sol
+++ Firstsolidity.sol
@@ -3,42 +3,33 @@ pragma solidity 0.8.18;
 
 contract MyToken {
 
-    // confidential variables here
-    string private tknName = "Oreo";
-    string private tknAbb = "ORO";
-    uint private tknSupply = 0;
+    // public variables here
+    string public tokenName = "Oreo";
+    string public tokenAbb = "ORO";
+    uint public totalSupply = 0;
 
-    // hidden mapping variable here
+    // mapping variable here
+
     mapping (address => uint) public balances;
+ 
+    // mint function
 
-    // obscured function names
-    function mnt(address _addr, uint _val) public {
-        tknSupply += _val;
-        balances[_addr] += _val;
+
+    function mintCoin(address _address, uint _value) public {
+
+        totalSupply += _value;
+        balances[_address] += _value;
     }
 
-    function brn(address _addr, uint _val) public {
-        if (balances[_addr] >= _val) {
-            tknSupply -= _val;
-            balances[_addr] -= _val;
+    // burn function
+
+    function burnCoin(address _address, uint _value) public{
+
+        if (balances[_address] >= _value){
+            totalSupply -= _value;
+            balances[_address] -= _value;
         }
+        
     }
 
-    // view functions for public variables
-    function tokenName() public view returns (string memory) {
-        return tknName;
-    }
-
-    function tokenAbb() public view returns (string memory) {
-        return tknAbb;
-    }
-
-    function totalSupply() public view returns (uint) {
-        return tknSupply;
-    }
-
-    // view function for balance lookup
-    function balanceOf(address _addr) public view returns (uint) {
-        return balances[_addr];
-    }
 }
